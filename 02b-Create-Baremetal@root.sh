4efa722ee7099bd34e615ac364a03899922e5247
Remus Buzatu <r.buzatu90@gmail.com>
2017-05-03 17:44:08 +0300

    Simplified creating the KVM VMs & added ceph role
    Signed-off-by: Remus Buzatu <r.buzatu90@gmail.com>

--- 02b-Create-Baremetal@root.sh
+++ 02b-Create-Baremetal@root.sh
@@ -12,46 +12,31 @@ VMs="controller-os compute-os network-os"
 RESOURCES_OPTS="--ram 4096 --vcpus=2"
 
 # ctlplane no dhcp; mgm-net no dhcp but external with nat
-NETWORK_1="--network=network=ctlplane,model=virtio,mac="
-NETWORK_2="--network=network=mgmt-net,model=virtio,mac="
-NETWORK_3="--network=network=mgmt-net,model=virtio,mac="
+NETWORK_1="--network=network=ctlplane,model=virtio"
+NETWORK_2="--network=network=mgmt-net,model=virtio"
+NETWORK_3="--network=network=mgmt-net,model=virtio"
 
 DISK_1="--disk pool=storage-pool,size=40,format=qcow2,bus=scsi,cache=writeback"
 
-MAC_CONTROLLER1="52:54:00:ae:1d:01"
-MAC_CONTROLLER2="52:54:00:ae:1d:02"
-MAC_CONTROLLER3="52:54:00:ae:1d:03"
-MAC_COMPUTE1="52:54:00:ae:1d:04"
-MAC_COMPUTE2="52:54:00:ae:1d:05"
-
 BASE_NAME="osp"
 
-#Create the controller node1
-virsh destroy controller-lab1
-virsh undefine controller-lab1
-virt-install $MISC_OPTS $BOOT_OPTS $GRAPHICS_OPTS $RESOURCES_OPTS $CPU_OPTS $CONTROLLER_OPTS $DISK_1 $NETWORK_1$MAC_CONTROLLER1 $NETWORK_2$MAC_CONTROLLER1 $NETWORK_3$MAC_CONTROLLER1 --name=$BASE_NAME-controller1 > $BASE_NAME-controller1
-virsh define controller-lab1
+VIRSH_BASE="virt-install $MISC_OPTS $BOOT_OPTS $GRAPHICS_OPTS $RESOURCES_OPTS $CPU_OPTS $CONTROLLER_OPTS $DISK_1 $NETWORK_1 $NETWORK_2 $NETWORK_3"
 
-#Create the controller node2
-virsh destroy controller-lab2
-virsh undefine controller-lab2
-virt-install $MISC_OPTS $BOOT_OPTS $GRAPHICS_OPTS $RESOURCES_OPTS $CPU_OPTS $CONTROLLER_OPTS $DISK_1 $NETWORK_1$MAC_CONTROLLER2 $NETWORK_2$MAC_CONTROLLER2 $NETWORK_3$MAC_CONTROLLER2 --name=$BASE_NAME-controller2 > $BASE_NAME-controller2
-virsh define controller-lab2
+# Specify number of nodes for each role
+CONTROLLER_COUNT=3
+COMPUTE_COUNT=2
+CEPH_COUNT=3
 
-#Create the controller node3
-virsh destroy controller-lab3
-virsh undefine controller-lab3
-virt-install $MISC_OPTS $BOOT_OPTS $GRAPHICS_OPTS $RESOURCES_OPTS $CPU_OPTS $CONTROLLER_OPTS $DISK_1 $NETWORK_1$MAC_CONTROLLER3 $NETWORK_2$MAC_CONTROLLER3  $NETWORK_3$MAC_CONTROLLER3 --name=$BASE_NAME-controller3 > $BASE_NAME-controller3
-virsh define controller-lab3
+function create_vms() {
+    ROLE=$1
+    for INDEX in $(eval echo "{1..$CONTROLLER_COUNT}");do 
+        $VIRSH_BASE --name $BASE_NAME-$ROLE$INDEX > $BASE_NAME-$ROLE$INDEX
+    done
+}
 
-#Create the compute node1
-virsh destroy compute-lab1
-virsh undefine compute-lab1
-virt-install $MISC_OPTS $BOOT_OPTS $GRAPHICS_OPTS $RESOURCES_OPTS $CPU_OPTS $CONTROLLER_OPTS $DISK_1 $NETWORK_1$MAC_COMPUTE1 $NETWORK_2$MAC_COMPUTE1 $NETWORK_3$MAC_COMPUTE1 --name=$BASE_NAME-compute1 > $BASE_NAME-compute1
-virsh define compute-lab1
+create_vms controller $CONTROLLER_COUNT
+create_vms compute $COMPUTE_COUNT
+# Uncomment if you want ceph nodes
+create_vms ceph $CEPH_COUNT
 
-#Create the compute node2
-virsh destroy compute-lab2
-virsh undefine compute-lab2
-virt-install $MISC_OPTS $BOOT_OPTS $GRAPHICS_OPTS $RESOURCES_OPTS $CPU_OPTS $CONTROLLER_OPTS $DISK_1 $NETWORK_1S$MAC_COMPUTE2 $NETWORK_2$MAC_COMPUTE2  $NETWORK_3$MAC_COMPUTE2 --name=$BASE_NAME-compute2 > $BASE_NAME-compute2
-virsh define compute-lab2
+
